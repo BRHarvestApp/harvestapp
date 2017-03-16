4b33051a3023732dc20f702e1b8527f791cf55a6
Terry Won <wont@email.chop.edu>
2017-03-16 15:08:16 -0700

    removed comments

--- popup.js
+++ popup.js
@@ -3,8 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
 	if (goToIssuesButton) {
 		goToIssuesButton.addEventListener("click", goToIssuesSite, false);
 	}
-	// TODO: We would like to bind this function to Elliot's menu option
-	//sendIssueDetailAndStartTimer();
 });
 
 function goToIssuesSite() {
